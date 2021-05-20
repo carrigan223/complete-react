f2844623e63a195e037366f7d5a42659f9787551
Andrew Carrigan <carrigan223@gmail.com>
2021-05-20 16:29:43 -0700

    33 card list component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import CardList from './components/card-list/card-list'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            users: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => this.setState({ users: users }))
+    }
+
+    render() {
+        console.log(this.state.users)
+        return (
+            <div className="App">
+                <div>
+                    {this.state.users.map(({ id, name, username }) => (
+                        <div key={id}>
+                            <CardList id={id} />
+                            <h1>{name}</h1>
+                            <h2>{username}</h2>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
