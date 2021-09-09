7ee044dbd9c039b15bfecbf006c6b8d6ccb12f17
Andrew Carrigan <carrigan223@gmail.com>
2021-09-09 12:01:17 -0700

    about to delete

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -4,11 +4,15 @@ import './card.styles.css'
 const Card = (props) => (
     <div className="card-container">
         <img
+            class="image"
             alt="monster"
             src={`https://robohash.org/${props.user.id}?set=set2`}
         />
+        <div>
         <h1>{props.user.name}</h1>
         <p>{props.user.email}</p>
+        </div>
+       
     </div>
 )
 
